28838a65c5b1c07c0b3cb7fa553e80a8b72f73b7
student <student@mini-10.local>
2022-03-17 11:13:26 +0100

    Dodanie trygonometrii

---
+++ sr/Trygonometria/trygonometria.cpp
@@ -0,0 +1,6 @@
+#include "trygonometria.h"
+#include <cmath>
+double degreemath::sin(double degree) {
+         //zamiana stopni na radiany:  pi radianów = 180 stopni
+         //return std::sin(radian);   //funkcja biblioteczna z piku <cmath>
+}
